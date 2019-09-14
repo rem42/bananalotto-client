d8e0820e9b4d60ebf143607e91e6675f8597d799
rem42 <remy492@hotmail.fr>
2019-09-14 17:45:52 +0200

    add test on cookieExtractor

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ A client for bananalotto using node
 [![Build Status](https://travis-ci.org/rem42/bananalotto-client.svg?branch=master)](https://travis-ci.org/rem42/bananalotto-client)
 [![Coverage Status](https://coveralls.io/repos/github/rem42/bananalotto-client/badge.svg?branch=master)](https://coveralls.io/github/rem42/bananalotto-client?branch=master)
 [![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2Frem42%2Fbananalotto-client.svg?type=shield)](https://app.fossa.io/projects/git%2Bgithub.com%2Frem42%2Fbananalotto-client?ref=badge_shield)
+[![install size](https://packagephobia.now.sh/badge?p=bananalotto-client)](https://packagephobia.now.sh/result?p=bananalotto-client)
+[![Greenkeeper badge](https://badges.greenkeeper.io/rem42/bananalotto-client.svg)](https://greenkeeper.io/)
 
 ## Installation 
 ```sh
@@ -26,4 +28,4 @@ BananalottoClient.init(credential)
 
 
 ## License
-[![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2Frem42%2Fbananalotto-client.svg?type=large)](https://app.fossa.io/projects/git%2Bgithub.com%2Frem42%2Fbananalotto-client?ref=badge_large) [![Greenkeeper badge](https://badges.greenkeeper.io/rem42/bananalotto-client.svg)](https://greenkeeper.io/)
+[![FOSSA Status](https://app.fossa.io/api/projects/git%2Bgithub.com%2Frem42%2Fbananalotto-client.svg?type=large)](https://app.fossa.io/projects/git%2Bgithub.com%2Frem42%2Fbananalotto-client?ref=badge_large)
--- src/Utils/cookie-extractor.spec.ts
+++ src/Utils/cookie-extractor.spec.ts
@@ -5,37 +5,53 @@ import {CookieExtractor} from './cookie-extractor';
 
 describe('test cookieExtractor class', () => {
     it('when empty cookie', () => {
-        const result = CookieExtractor.getCookiesString([]);
+        const testData: string[] = [];
+        const isGoodCookies = CookieExtractor.isContainAllCookies(testData);
+        expect(isGoodCookies).to.equal(false);
+
+        const result = CookieExtractor.getCookiesString(testData);
         expect(result).to.equal('');
     });
     it('return empty when cookie not in list', () => {
-        const result = CookieExtractor.getCookiesString([
+        const testData: string[] = [
             'foo=bar; bar=foo'
-        ]);
+        ];
+        const isGoodCookies = CookieExtractor.isContainAllCookies(testData);
+        expect(isGoodCookies).to.equal(false);
+        const result = CookieExtractor.getCookiesString(testData);
         expect(result).to.equal('');
     });
     it('return all cookie in list', () => {
-        const result = CookieExtractor.getCookiesString([
+        const testData: string[] = [
             'PHPSESSID=bar;',
             'uid=foo;',
             'pid=baz;',
             'cid=faa;',
-        ]);
+        ];
+        const isGoodCookies = CookieExtractor.isContainAllCookies(testData);
+        expect(isGoodCookies).to.equal(true);
+        const result = CookieExtractor.getCookiesString(testData);
         expect(result).to.equal('PHPSESSID=bar; uid=foo; pid=baz; cid=faa');
     });
     it('not return cookie deleted', () => {
-        const result = CookieExtractor.getCookiesString([
+        const testData: string[] = [
             'PHPSESSID=bar;',
             'uid=deleted;'
-        ]);
+        ];
+        const isGoodCookies = CookieExtractor.isContainAllCookies(testData);
+        expect(isGoodCookies).to.equal(false);
+        const result = CookieExtractor.getCookiesString(testData);
         expect(result).to.equal('PHPSESSID=bar');
     });
     it('return cookie with value not deleted', () => {
-        const result = CookieExtractor.getCookiesString([
+        const testData: string[] = [
             'PHPSESSID=deleted;',
             'PHPSESSID=bar;',
             'PHPSESSID=deleted;',
-        ]);
+        ];
+        const isGoodCookies = CookieExtractor.isContainAllCookies(testData);
+        expect(isGoodCookies).to.equal(false);
+        const result = CookieExtractor.getCookiesString(testData);
         expect(result).to.equal('PHPSESSID=bar');
     });
 });
